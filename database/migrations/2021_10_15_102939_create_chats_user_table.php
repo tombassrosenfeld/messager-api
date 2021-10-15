34d754ac1ad34a938e6ae644b3be41a6c83a461c
Tom Rosenfeld <thomasrosenfeld@hotmail.com>
2021-10-15 11:33:10 +0100

    created pivot table for chats_user

---
+++ database/migrations/2021_10_15_102939_create_chats_user_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChatsUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chats_user', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("user_id")
+            ->constrained()
+            ->onDelete("cascade");
+        $table->foreignId("chat_id")
+            ->constrained()
+            ->onDelete("cascade");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chats_user');
+    }
+}
