63e16e5199077beccab81575a3cdfdf112793d77
Tom Rosenfeld <thomasrosenfeld@hotmail.com>
2021-10-15 12:03:47 +0100

    corrected pivot table name typo

--- database/migrations/2021_10_15_102939_create_chat_user_table.php
+++ database/migrations/2021_10_15_102939_create_chat_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateChatsUserTable extends Migration
+class CreateChatUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateChatsUserTable extends Migration
      */
     public function up()
     {
-        Schema::create('chats_user', function (Blueprint $table) {
+        Schema::create('chat_user', function (Blueprint $table) {
             $table->id();
             $table->foreignId("user_id")
             ->constrained()
